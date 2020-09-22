5a922a4fee671d5962a6d9433b5ef6e8dc985157
ChandraMohanG1993 <chandra4ug@gmail.com>
2020-09-22 13:01:42 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { HeaderComponent } from './header/header.component';
 import { Section1Component } from './section1/section1.component';
 import { Section2Component } from './section2/section2.component';
 import { Section3Component } from './section3/section3.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
     HeaderComponent,
+    HomeComponent,
     Section1Component,
     Section2Component,
     Section3Component,   
